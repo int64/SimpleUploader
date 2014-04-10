f0cce20d24ed49ef20eddbf241245c560b7180dc
int64 <maggotjb@gmail.com>
2014-04-10 14:23:43 +0600

    update example

--- simpleuploader.js
+++ simpleuploader.js
@@ -14,17 +14,8 @@ new Uploader({
 	event: 'click',		// jQuery event to target for start upload (not required, by default 'click')
 	params: { id: 1 },	// POST params to send (not required)
 	multiple: true,		// multiple file choose (not required, by default 'false')
-						// function to call after load (not required)
-	load: function(response){
-		var src = $.parseJSON(response.target.responseText).src;
-		_c(src);
-		//var image = response.src;
-		$("#upload").css({
-			'background': 'url("' + src + '") center',
-			'height': '186px',
-			'background-size': 'cover',
-		});
-	}
+	load: function(response){},	// function to call after load (not required)
+	progress: function(response){},	// function to call on load progress (not required)
 });
 
 */
